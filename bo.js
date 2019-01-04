d38a61f69d949eb9966e7064838823f15694e107
Mustafa Barodawala <itsjustmustafa@gmail.com>
2019-01-05 02:42:43 +1100

    added to define words using owlbot-dictionary, and added hi to greetings
    also fixed up some typos with double dots after toLowerCase
    
    i dont know if this will work due to all the asynchronicity, but fingers crossed

--- bo.js
+++ bo.js
@@ -2,6 +2,8 @@ var fs = require('fs');
 const shell = require('shelljs');
 
 const login = require("facebook-chat-api");
+const dict = require("owlbot-dictionary");
+
 var user = fs.readFileSync("username.txt", "utf8");
 var pass = fs.readFileSync("password.txt", "utf8");
 
@@ -17,16 +19,32 @@ login({email: user, password: pass}, (err, api) => {
             api.sendMessage("🤠", message.threadID); //not sure if this will work, as its an emoji
         }
         //good-night message
-        if (message.body.toLowerCase..match(/go[o]+d[\s|-]+night bo/g) !== null) {
+        if (message.body.toLowerCase.match(/go[o]+d[\s|-]+night bo/g) !== null) {
             api.sendMessage("goodnight 🌃", message.threadID);
         }
         //good-morning message
-        if (message.body.toLowerCase..match(/go[o]+d[\s|-]+morning bo/g) !== null) {
+        if (message.body.toLowerCase.match(/go[o]+d[\s|-]+morning bo/g) !== null) {
             api.sendMessage("goodmorning 🌞", message.threadID);
         }
         //geeting message
         if (message.body.toLowerCase.match(/(hey|hello|yo|wassup)\sbo/g) !== null) {
             api.sendMessage("hi!", message.threadID);
+        }        
+        if (message.body.toLowerCase.match(/bo\sdefine\s([^\s]+)/g) !== null) {
+            r = /bo\sdefine\s([^\s]*)/;
+            word = r.exec(message.body.toLowerCase())[1];
+            
+            msg = dict.define(word).then(function(def){
+                if(def.length == 0){
+                    api.sendMessage("i dont know what "+word+" means 😥");
+                }else{
+                    api.sendMessage(word+": "+def[0].defenition, message.threadID);
+                }
+            });
+            
+            dict.define(word).then(function(def){
+                api.sendMessage(word+": "+def[0].defenition, message.threadID);
+            });
         }
         if (message.body.toLowerCase() === "rip") {
             api.sendMessage("rip", message.threadID);
